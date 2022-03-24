b4d9dd288d808ecb222ef24b163fc18ce1378cae
Kimberly West <kdenton993@gmail.com>
2022-03-23 20:00:41 -0600

    changes in html

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,30 +1,22 @@
 // create variables and grab by id
-var books = document.getElementById("books")
-var beer = document.getElementById("beer")
-
+var books = document.getElementById("books");
+var beer = document.getElementById("beer");
+ 
 //API URL for google books.
 let apiUrl = "https://www.googleapis.com/books/v1/volumes?q=search+terms";
 
 // API Key for google books
-let APIkey = "AIzaSyAKZodEhFbkVKGFt3yykb7Sxeem5UZ0Ni0"
+let APIkey = "AIzaSyAKZodEhFbkVKGFt3yykb7Sxeem5UZ0Ni0";
 
 // make request to url
-fetch(apiUrl)
-        
-.then(function(response) {
-// request was successful
-    if (response.ok) {
-        response.json().then(function(data) {
-            displadyBooks(data);
-        });
-    // request fails
-    } else {
-        alert("Error: " + response.statusText);
-    }
-})  
+fetch(apiUrl).then(function (response) {
+  // request was successful
+  if (response.ok) {
+    response.json().then(function (data) {
+      displadyBooks(data);
+    });
+  }
+});
 
-.catch(function(error) {
-    alert("Unable to connect to Google Books");
-})
-
-$("#")
+$("#books").submit(searchSubmitHandler);
+$("#beer").on("click", function (event) {});
