5f819760700c8de51983c1316c18567416de29ad
Kimberly West <kdenton993@gmail.com>
2022-03-28 19:52:49 -0600

    made changes to js on ul and li added hr to result box

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Project-1
+# Books and Beer API. An awesome search page to find your favorite book while a random beer recommendation generates on the page. 
+
+## Built with Javascript, HTML, CSS, Tailwind!
+
+![alt-text](assets/images/screenshot)
+
+### 
+Contributions by Matthew Songer, Nathanial Shattuck, Kimberly West, Matthew Boisse 
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,57 +1,61 @@
 // Event listeners for the search button
-document.querySelector('#submit-button').addEventListener('click', searchBook);
+document.querySelector("#submit-button").addEventListener("click", searchBook);
 
 // When user inputs the search bar and clicked search button, start searching
 function searchBook(event) {
-	let query = document.querySelector('#search-input').value;
-	// Initiate the fetch API
-	fetch(`https://www.googleapis.com/books/v1/volumes?q=${query}`)
-      .then((res) => res.json())
-      .then((data) => {
-      	// Filter the search results by author, title, and subtitle
-      	let output = '';
-      	data.items.forEach(book => {
-      		output += `
+  let query = document.querySelector("#search-input").value;
+  // Initiate the fetch API
+  fetch(`https://www.googleapis.com/books/v1/volumes?q=${query}`)
+    .then((res) => res.json())
+    .then((data) => {
+      // Filter the search results by author, title, and subtitle
+      let output = "";
+      data.items.forEach((book) => {
+        output += `
           <div class="results-box">
       		<ul>
-            <img src= ${book.volumeInfo.imageLinks.smallThumbnail}>
-	      		<li><b>Title:</b> ${book.volumeInfo.title}</li>
-	      		<li><b>Author:</b> ${book.volumeInfo.authors}</li>
-            <a href=${book.volumeInfo.canonicalVolumeLink}><button class="book-info"><b>See this book</b></button></a>
-      		</ul>
+           		<li>
+					<img src= ${book.volumeInfo.imageLinks.smallThumbnail}/>
+	      			<p><b>Title:</b> ${book.volumeInfo.title}</p>
+	      			<p><b>Author:</b> ${book.volumeInfo.authors}</p>
+            		<a href=${book.volumeInfo.canonicalVolumeLink}><button class="book-info"><b>See this book</b></button></a>
+					</li>
+					</ul>
+					<hr>
           </div>
       		`;
-      	});
-      	// Display the search result
-    		document.querySelector("#output").innerHTML = output;
-      })
-      // Throw an error message when something went wrong during the search
-      .catch((error) => console.log(error));
+      });
+      // Display the search result
+      document.querySelector("#output").innerHTML = output;
+    })
+    // Throw an error message when something went wrong during the search
+    .catch((error) => console.log(error));
 
-      // Display "Search results of" text when clicked the search button
-	  document.querySelector('.text').innerHTML = `Search results for: "${query}"`
-	}
+  // Display "Search results of" text when clicked the search button
+  document.querySelector(".text").innerHTML = `Search results for: "${query}"`;
+}
 
 // Start searching the lists when user clicks entry key
-document.querySelector("#search-input").addEventListener("keyup", function(event) {
+document
+  .querySelector("#search-input")
+  .addEventListener("keyup", function (event) {
     if (event.keyCode === 13) {
-        document.querySelector("#submit-button").click();
+      document.querySelector("#submit-button").click();
     }
-});
-
+  });
 
 /*
 fetch("https://www.thecocktaildb.com/api/json/v1/1/random.php")
   .then((response) => {
-    if (response.ok) {
-      return response.json();
-    } else {
-      throw new Error("NETWORK RESPONSE ERROR");
-    }
+	if (response.ok) {
+	  return response.json();
+	} else {
+	  throw new Error("NETWORK RESPONSE ERROR");
+	}
   })
   .then(data => {
-    console.log(data);
-    displayCocktail(data)
+	console.log(data);
+	displayCocktail(data)
   })
   .catch((error) => console.error("FETCH ERROR:", error));
   function displayCocktail(data) {
@@ -88,29 +92,29 @@ fetch("https://www.thecocktaildb.com/api/json/v1/1/random.php")
   }   
   */
 
-  document.querySelector('#submit-button').addEventListener('click', getBeer);
+document.querySelector("#submit-button").addEventListener("click", getBeer);
 
-function getBeer(){
-	var beerApi = "https://api.punkapi.com/v2/beers/random";
-	fetch(beerApi).then(function(response) {
-	// request was successful
-	  if (response.ok) {
-		response.json().then(function(data) {
-		//Get values for each Name, desc, ABV, and Img of beers
-			var beerName = data[0].name;
-			var beerDescription = data[0].description;
-			var beerAbv = data[0].abv
-			var beerImg = data[0].image_url
-			console.log(beerDescription)
-			document.querySelector("#beer-title").innerHTML=beerName;
-			document.querySelector("#beer-description").innerHTML=beerDescription;
-			document.querySelector("#beer-abv").innerHTML="Alchhol by Volume="+beerAbv;
-			document.getElementById("beer-img").src = beerImg; 
-		});
-	  }
-	  else {
-	  //If not successful redirect back to homepage
-	  document.location.replace("./index.html");
-	  }
-});
+function getBeer() {
+  var beerApi = "https://api.punkapi.com/v2/beers/random";
+  fetch(beerApi).then(function (response) {
+    // request was successful
+    if (response.ok) {
+      response.json().then(function (data) {
+        //Get values for each Name, desc, ABV, and Img of beers
+        var beerName = data[0].name;
+        var beerDescription = data[0].description;
+        var beerAbv = data[0].abv;
+        var beerImg = data[0].image_url;
+        console.log(beerDescription);
+        document.querySelector("#beer-title").innerHTML = beerName;
+        document.querySelector("#beer-description").innerHTML = beerDescription;
+        document.querySelector("#beer-abv").innerHTML =
+          "Alchhol by Volume=" + beerAbv;
+        document.getElementById("beer-img").src = beerImg;
+      });
+    } else {
+      //If not successful redirect back to homepage
+      document.location.replace("./index.html");
+    }
+  });
 }
